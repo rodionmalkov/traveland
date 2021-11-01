9053b22d5f9129dd8b4f247013f91ec492441fbc
Rodion <rodionmalkov@gmail.com>
2021-11-01 19:48:05 +0500

    Cleaned up in JS

---
+++ js/component/swiper.js
@@ -0,0 +1,47 @@
+const swiper = new Swiper(".swiper", {
+  slidesPerView: 5,
+  slidesPerGroup: 4,
+  loopFillGroupWithBlank: true,
+  mousewheel: true,
+
+  navigation: {
+    nextEl: ".slider-control__next",
+    prevEl: ".slider-control__prev",
+  },
+
+  pagination: {
+    el: ".slider-control__pagination",
+    type: "bullets",
+    clickable: true,
+  },
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+      slidesPerGroup: 1,
+    },
+
+    1000: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      slidesPerGroup: 4,
+    },
+
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 50,
+      slidesPerGroup: 4,
+    },
+
+    1600: {
+      slidesPerView: 5,
+      spaceBetween: 74,
+      slidesPerGroup: 4,
+    },
+  },
+});
---
+++ js/sample/social.js
@@ -0,0 +1,22 @@
+const socialLinks = [
+  {
+    site: "facebook",
+    link: "https://www.facebook.com/",
+    icon: "./img/icons/social-facebook.jpg",
+  },
+  {
+    site: "instagram",
+    link: "https://www.instagram.com/",
+    icon: "./img/icons/social-instagram.jpg",
+  },
+  {
+    site: "twitter",
+    link: "https://www.twitter.com/",
+    icon: "./img/icons/social-twitter.jpg",
+  },
+  {
+    site: "google",
+    link: "https://www.google.com/",
+    icon: "./img/icons/social-google.jpg",
+  },
+];
